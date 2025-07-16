d241da3e185f8afc26c3797066af4947caecfc6c
Carmen Castro <carmencastro@Carmens-MacBook-Pro.local>
2025-07-16 20:59:07 +0100

    Fixing format

--- python/wait-for-nifi.sh
+++ python/wait-for-nifi.sh
@@ -1,14 +1,7 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2822
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
-
-\f0\fs24 \cf0 #!/bin/bash\
-echo "Waiting for NiFi to be ready..."\
-until curl -f http://nifi:8080/nifi/; do\
-  echo "NiFi is not ready yet..."\
-  sleep 5\
-done\
-echo "NiFi is ready!"}
+#!/bin/bash
+echo "Waiting for NiFi to be ready..."
+until curl -f http://nifi:8080/nifi/; do
+  echo "NiFi is not ready yet..."
+  sleep 5
+done
+echo "NiFi is ready!"
